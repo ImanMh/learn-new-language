e9c7728219bb26080f5fb66ccbf12a46533512ec
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2019-04-26 19:09:04 +0430

    adds add new word layout

--- screens/AddNewTranslation.js
+++ screens/AddNewTranslation.js
@@ -1,11 +1,55 @@
 import React from 'react';
 import {
-  Platform,
   ScrollView,
   StyleSheet,
   Text,
   View,
+  TextInput,
+  Button,
+  FlatList,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    paddingHorizontal: 10,
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  inputSection: {
+    paddingVertical: 10,
+    position: 'relative',
+  },
+  multilineInput: {
+    backgroundColor: '#fff',
+    height: 80,
+    padding: 10,
+  },
+  saveButtonWrapper: {
+    backgroundColor: '#bbb',
+    position: 'absolute',
+    bottom: 15,
+    right: 15,
+    minWidth: 70,
+  },
+  suggestedTranslations: {
+
+  },
+  suggestedTranslationsTitle: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  suggestedTranslationItem: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    height: 45,
+    display: 'flex',
+    justifyContent: 'center',
+    paddingLeft: 10,
+  },
+});
 
 export default class AddNewTranslation extends React.Component {
   static navigationOptions = {
@@ -14,122 +58,58 @@ export default class AddNewTranslation extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        </ScrollView>
-      </View>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}
+      >
+        <View
+          style={styles.inputSection}
+        >
+          <TextInput
+            placeholder="Add the new word..."
+            style={styles.multilineInput}
+            multiline
+            onChangeText={(newWord) => { this.setState({ newWordInput: newWord }); }}
+          />
+        </View>
+        <View
+          style={styles.inputSection}
+        >
+          <TextInput
+            placeholder="Add translation..."
+            style={styles.multilineInput}
+            multiline
+            onChangeText={(newWord) => { this.setState({ translationInput: newWord }); }}
+          />
+          <View style={styles.saveButtonWrapper}>
+            <Button
+              color="#555"
+              title="Add"
+              onPress={() => { console.log('add the save handler.'); }}
+            />
+          </View>
+        </View>
+        <View
+          style={styles.suggestedTranslations}
+        >
+          <Text style={styles.suggestedTranslationsTitle}>Suggested Translations</Text>
+          <FlatList
+            data={[
+              { value: 'sib', id: 1 },
+              { value: 'sibe ghermez', id: 2 },
+              { value: 'sibe sabz', id: 3 },
+            ]}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => (
+              <View
+                style={styles.suggestedTranslationItem}
+              >
+                <Text>{item.value}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </ScrollView>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});
